0145d2c3e60e357f56a26412b6feec305e27b8c7
Henrik Olsson <henrik.olsson@bokusgruppen.com>
2024-01-29 09:37:13 +0100

    List dirs

--- list_prg.php
+++ list_prg.php
@@ -1,15 +1,26 @@
 <?php
-$directory = "."; // Replace with your directory path
+// Default directory path
+$rootDirectory = "."; // Replace with your root directory path
+$currentDirectory = isset($_GET['dir']) ? $_GET['dir'] : $rootDirectory;
 
-// Open a directory, and read its contents
-if (is_dir($directory)){
-  if ($dh = opendir($directory)){
-    while (($file = readdir($dh)) !== false){
-      if (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
-        echo "<a href='$directory/$file'>$file</a><br>";
-      }
+// Function to list directories and .prg files
+function listDir($dir) {
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                if ($file != "." && $file != "..") {
+                    if (is_dir("$dir/$file")) {
+                        echo "<a href='?dir=" . urlencode("$dir/$file") . "'>$file</a><br>";
+                    } elseif (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
+                        echo "<a href='$dir/$file'>$file</a><br>";
+                    }
+                }
+            }
+            closedir($dh);
+        }
     }
-    closedir($dh);
-  }
 }
+
+// List directories and .prg files in the current directory
+listDir($currentDirectory);
 ?>
