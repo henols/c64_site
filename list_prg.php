0d277e2d733b73af64816694c64c68c82d988319
Henrik Olsson <henrik.olsson@bokusgruppen.com>
2024-01-29 10:03:59 +0100

    Sort

--- list_prg.php
+++ list_prg.php
@@ -3,22 +3,28 @@
 $rootDirectory = "."; // Replace with your root directory path
 $currentDirectory = isset($_GET['dir']) ? $_GET['dir'] : $rootDirectory;
 
-// Function to check if directory contains .prg files
-function hasPrgFiles($dir) {
-    $hasPrg = false;
+// Configurable file types
+$fileTypes = ["prg", "d64"]; // Add more file types as needed
+
+// Function to check if directory contains files of specified types
+function hasSpecifiedFiles($dir, $types) {
+    $hasFile = false;
     if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false && !$hasPrg) {
-            if (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
-                $hasPrg = true;
+        while (($file = readdir($dh)) !== false && !$hasFile) {
+            if (in_array(pathinfo($file, PATHINFO_EXTENSION), $types)) {
+                $hasFile = true;
             }
         }
         closedir($dh);
     }
-    return $hasPrg;
+    return $hasFile;
 }
 
-// Function to list directories that contain .prg files and .prg files themselves
-function listDir($dir, $root) {
+// Function to list directories and files
+function listDir($dir, $root, $types) {
+    $directories = [];
+    $files = [];
+
     if (is_dir($dir)) {
         if ($dh = opendir($dir)) {
             // Link to parent directory if not in root
@@ -29,18 +35,29 @@ function listDir($dir, $root) {
 
             while (($file = readdir($dh)) !== false) {
                 if ($file != "." && $file != "..") {
-                    if (is_dir("$dir/$file") && hasPrgFiles("$dir/$file")) {
-                        echo "<a href='?dir=" . urlencode("$dir/$file") . "'>$file</a><br>";
-                    } elseif (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
-                        echo "<a href='$dir/$file'>$file</a><br>";
+                    if (is_dir("$dir/$file") && hasSpecifiedFiles("$dir/$file", $types)) {
+                        $directories[] = $file;
+                    } elseif (in_array(pathinfo($file, PATHINFO_EXTENSION), $types)) {
+                        $files[] = $file;
                     }
                 }
             }
             closedir($dh);
         }
     }
+
+    // Sort and display directories and files
+    sort($directories);
+    foreach ($directories as $directory) {
+        echo "<a href='?dir=" . urlencode("$dir/$directory") . "'>$directory</a><br>";
+    }
+
+    sort($files);
+    foreach ($files as $file) {
+        echo "<a href='$dir/$file'>$file</a><br>";
+    }
 }
 
-// List directories containing .prg files and .prg files in the current directory
-listDir($currentDirectory, $rootDirectory);
+// List directories containing specified file types and files in the current directory
+listDir($currentDirectory, $rootDirectory, $fileTypes);
 ?>
