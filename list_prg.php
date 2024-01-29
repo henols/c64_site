bb253aee7728bcb4af728c2befcdb3fe70d42076
Henrik Olsson <henrik.olsson@bokusgruppen.com>
2024-01-29 09:43:52 +0100

    s

--- list_prg.php
+++ list_prg.php
@@ -1,15 +1,29 @@
 <?php
 // Default directory path
-$rootDirectory = "."; // Replace with your root directory path
+$rootDirectory = "/path/to/directory"; // Replace with your root directory path
 $currentDirectory = isset($_GET['dir']) ? $_GET['dir'] : $rootDirectory;
 
-// Function to list directories and .prg files
+// Function to check if directory contains .prg files
+function hasPrgFiles($dir) {
+    $hasPrg = false;
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false && !$hasPrg) {
+            if (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
+                $hasPrg = true;
+            }
+        }
+        closedir($dh);
+    }
+    return $hasPrg;
+}
+
+// Function to list directories that contain .prg files and .prg files themselves
 function listDir($dir) {
     if (is_dir($dir)) {
         if ($dh = opendir($dir)) {
             while (($file = readdir($dh)) !== false) {
                 if ($file != "." && $file != "..") {
-                    if (is_dir("$dir/$file")) {
+                    if (is_dir("$dir/$file") && hasPrgFiles("$dir/$file")) {
                         echo "<a href='?dir=" . urlencode("$dir/$file") . "'>$file</a><br>";
                     } elseif (pathinfo($file, PATHINFO_EXTENSION) == "prg") {
                         echo "<a href='$dir/$file'>$file</a><br>";
@@ -21,6 +35,6 @@ function listDir($dir) {
     }
 }
 
-// List directories and .prg files in the current directory
+// List directories containing .prg files and .prg files in the current directory
 listDir($currentDirectory);
 ?>
